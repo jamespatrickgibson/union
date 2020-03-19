f8259cb65a934a1e66eb007c76e5430c34769622
James Patrick Gibson <james@jamespatrickgibson.com>
2020-03-19 18:34:46 -0400

    feat: Added Inline component. Added squish options to box

--- src/docs/components/DocsHeader/DocsHeader.svelte
+++ src/docs/components/DocsHeader/DocsHeader.svelte
@@ -1,6 +1,9 @@
 <script>
   import Box from "../../../components/Box/Box.svelte";
+  import Heading from "../../../components/Heading/Heading.svelte";
+  import Inline from "../../../components/Inline/Inline.svelte";
   import Text from "../../../components/Text/Text.svelte";
+  import Stack from "../../../components/Stack/Stack.svelte";
 </script>
 
 <style lang="scss">
@@ -8,21 +11,30 @@
 </style>
 
 <header>
-  <Box background="neutralLight" padding="l">
-    <Text>
-      Union. A design style boilerplate for creating scalable design
-      architecture.
-    </Text>
-    <nav>
-      <Text>Version 0.0.1</Text>
-      <Text>
-        <a href="https://github.com/jamespatrickgibson/union">View on Github</a>
-        <br />
-        <a
-          href="https://raw.githubusercontent.com/jamespatrickgibson/union/master/dist/css/union.css">
-          Download union.css
-        </a>
-      </Text>
-    </nav>
+  <Box background="dark" padding="l" squish>
+    <Stack>
+      <div>
+        <Heading size="1">union.</Heading>
+        <Text size="1">
+          A design style boilerplate for creating scalable design architecture.
+        </Text>
+        <Text size="3">Work in Progress • Version 0.0.1</Text>
+      </div>
+      <nav>
+        <Text size="3">
+          <Inline space="xs">
+            <a href="https://github.com/jamespatrickgibson/union">
+              View on Github
+            </a>
+            <a
+              href="https://raw.githubusercontent.com/jamespatrickgibson/union/master/dist/css/union.css">
+              Download union.css
+            </a>
+          </Inline>
+
+        </Text>
+        <Text size="3" />
+      </nav>
+    </Stack>
   </Box>
 </header>
