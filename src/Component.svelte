dee32da9074210159c3f246a7e2588fc2bf069e9
James Patrick Gibson <james@jamespatrickgibson.com>
2020-03-21 14:09:53 -0400

    chore: Added prettier rc

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,10 +1,12 @@
 <script>
+  /** @type {string} Name of the input */
   export let text = "Test Component";
 </script>
 
 <style>
   div {
     background: pink;
+    padding: 2rem;
   }
 </style>
 
