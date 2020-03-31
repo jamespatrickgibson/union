45b89af519209b2c602a81a4574021d614571948
James Patrick Gibson <james@jamespatrickgibson.com>
2020-03-31 19:00:22 -0400

    fix: Updated build issues

--- doczrc.js
+++ doczrc.js
@@ -2,6 +2,7 @@
 export default {
   description:
     "Union is a design system boilerplate and component library which is inspired by traditional graphic design theories and practices.",
+  dest: "/public",
   ignore: ["README.md", "CHANGELOG.md"],
   menu: ["Overview"],
   title: "Union Design System"
