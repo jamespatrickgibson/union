d95cc0b183dde14d65369a82c90581bfc2e51d80
James Patrick Gibson <james@jamespatrickgibson.com>
2020-04-11 18:11:32 -0400

    WIP: Added theme file, tried to get a prop table working

--- docs/src/components/Layout.jsx
+++ docs/src/components/Layout.jsx
@@ -1,15 +1,20 @@
-import React from "react"
-import { MDXProvider } from "@mdx-js/react"
-import { Link } from "gatsby"
-import { Box, Text, Stack } from "../../../union/components/components"
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
+import { Link } from "gatsby";
+import {
+  Box,
+  Heading,
+  Text,
+  Stack,
+} from "../../../union/components/components";
 
-const shortcodes = { Box, Text, Stack }
+const shortcodes = { Box, Heading, Text, Stack };
 
 const ListLink = (props) => (
   <li>
     <Link to={props.to}>{props.children}</Link>
   </li>
-)
+);
 
 export default ({ children }) => (
   <MDXProvider components={shortcodes}>
@@ -17,8 +22,12 @@ export default ({ children }) => (
       <ul>
         <ListLink to="/">Home</ListLink>
         <ListLink to="/about/">About</ListLink>
+        <ListLink to="/box/">Box</ListLink>
+        <ListLink to="/heading/">Heading</ListLink>
+        <ListLink to="/stack/">Stack</ListLink>
+        <ListLink to="/text/">Text</ListLink>
       </ul>
     </nav>
     <main>{children}</main>
   </MDXProvider>
-)
+);
