7c032301ebac5bab3abbd2aa8160b706602905e5
James Patrick Gibson <james@jamespatrickgibson.com>
2020-06-16 17:13:41 -0400

    feat: Added design for nonfunctional example fo example component

--- docs/components/Example.svelte
+++ docs/components/Example.svelte
@@ -1,1 +1,34 @@
-<div>Example Component</div>
+<script>
+	import Prism from "svelte-prism";
+	import ExampleCode from "./ExampleCode.svx";
+</script>
+
+<style lang="scss">
+	.example {
+		border: 0.0625rem solid var(--color-border);
+
+		&__preview {
+			padding: var(--space-3);
+			border-bottom: 0.0625rem solid var(--color-border);
+		}
+
+		// This resets some default style from Prism
+		&__code {
+			margin-top: -0.5em;
+			margin-bottom: -0.5em;
+		}
+	}
+</style>
+
+<div class="example">
+	<div class="example__preview">
+		<slot />
+	</div>
+	<div class="example__code">
+		<Prism language="html">
+			{`
+				<p>Sample Code</p>
+			`}
+		</Prism>
+	</div>
+</div>
