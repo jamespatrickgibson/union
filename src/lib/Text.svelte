ad3f9483e611b34e807793f234149c618f16cbf4
James Patrick Gibson <james@jamespatrickgibson.com>
2021-10-15 11:50:32 +0000

    Initial Commit

---
+++ src/lib/Text.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let tag = 'p';
+</script>
+
+{#if tag === 'h1'}
+	<h1><slot /></h1>
+{:else}
+	<p><slot /></p>
+{/if}
+
+<style lang="scss">
+	@use 'utils' as *;
+
+	@mixin text-reset() {
+		margin: unset;
+	}
+
+	h1 {
+		@include text-reset();
+		font-family: $font-family-default;
+		font-weight: $font-weight-normal;
+		color: $color-text-default;
+	}
+	p {
+		@include text-reset();
+		font-family: $font-family-default;
+		font-weight: $font-weight-normal;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Text } from '../lib/';
+</script>
+
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<Text tag="h1">Welcome to SvelteKit</Text>
+<Text>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</Text>
+
+<style lang="scss">
+	@use 'utils' as *;
+	:global(body) {
+		background: $color-background-body;
+		margin: 0;
+	}
+</style>
